3dc75e7b843aa2ff4eb8de56333faea93c3ade63
František Jahoda <frantisek.jahoda@gmail.com>
2018-04-15 23:15:07 +0200

    32 colors

--- write_map.py
+++ write_map.py
@@ -7,31 +7,49 @@ import PIL.ImageFilter
 import sys
 import math
 import struct
-import itertools
 
 DIAMETER = 136
-DIAMETER = int(136*1.8)
-COLORS = '''#000000
-#0d0a31
-#29390e
-#41501a
-#695c38
-#57636c
-#606a31
-#8e8d87
-#8394a4
-#a89060
-#aca9a2
-#cec8be
-#f6d89d
-#756fcc
+
+COLORS = [c for c in '''
+#000000
+#040531
+#0d0b37
+#15143c
+
+#28294d
+#153b06
+#fad698
+#464766
+
+#3c5316
+#2a5a10
+#5b5a2b
+#725c36
+
+#f7fcac
+#8086a2
 #000010
-#101010'''.split('\n')
+#09090a
 
+#a18256
+#c9cbd3
+#868898
+#84915a
 
-# 18 / 136 pixels can be compressed twice as much
-# probably not worthy of extra logic
-half_strip = int(DIAMETER / 2 * (1.0 - math.sqrt(1.0 - 0.5**2)))
+#d47b54
+#a6a494
+#c8a375
+#a7aab2
+
+#e5c49d
+#f4f5f3
+#cfcbc2
+#2a350c
+
+#64667f
+#596d32
+#f6eccd
+'''.split('\n') if c]
 
 
 def compute_palette(input_colors):
@@ -72,33 +90,34 @@ def convert_colors(source, colors):
     return source._new(target_im)
 
 
-def compress_rows(source):
+def compress_bits(source):
     w, h = source.size
     data = source.load()
-    row_data = []
-    for y in range(h):
-        for key, groups in itertools.groupby(data[x, y] for x in range(w)):
-            c = len(list(groups))
-            while c > 0:
-                if c > 16:
-                    row_data.append((key, 15))
-                    c -= 16
-                else:
-                    row_data.append((key, c - 1))
-                    c = 0
-    print('original size', w * h)
-    print('compressed size', len(row_data))
-    return row_data
+    data = bytes([data[x, y] for y in range(h) for x in range(w)])
+    res = []
+    for d in data:
+        res.append(d & 3)
+        res.append(d >> 2 & 3)
+        res.append(d >> 4 & 3)
+    res.extend([0] * (4 - len(res) % 4))  # make len(res) divisible by 4
+    result = []
+    for i in range(0, len(res), 4):
+        result.append(res[i] | res[i+1] << 2 | res[i+2] << 4 | res[i+3] << 6)
+    return result
 
 
-PALETTE_TYPE = 12
-MAP_TYPE = 4
+CHUNK_PALETTE = 12
+PALETTE_SIZE = 48
+CHUNK_TILES = 1
+CHUNK_SPRITES = 2
+CHUNK_MAP = 4
+WRITE_CHUNKS = (CHUNK_PALETTE, CHUNK_MAP, CHUNK_TILES, CHUNK_SPRITES)
 
 
 def write_cartridge(filename, data, colors):
     with open('temp.tic', 'wb') as fout:
         for ch_type, chunk_bytes, payload in read_cartridge(filename):
-            if ch_type not in (PALETTE_TYPE, MAP_TYPE):
+            if ch_type not in WRITE_CHUNKS:
                 fout.write(chunk_bytes)
                 fout.write(payload)
         write_palette(fout, colors)
@@ -106,18 +125,45 @@ def write_cartridge(filename, data, colors):
 
 
 def write_palette(fout, colors):
-    fout.write(struct.pack('I', 48 << 8 | PALETTE_TYPE))
-    fout.write(bytes(compute_palette(COLORS)[:48]))
+    fout.write(struct.pack('I', 48 << 8 | CHUNK_PALETTE))
+    fout.write(bytes(compute_palette(COLORS)[PALETTE_SIZE:2*PALETTE_SIZE]))
 
 
 def write_rows(fout, rows):
-    size = (1 +  # DIAMETER size
-            len(rows))  # one byte for each tuple in row
-    fout.write(struct.pack('I', size << 8 | MAP_TYPE))
+    data_size = len(rows)
+    size = (2 +  # height
+            2 +  # data size
+            PALETTE_SIZE*2)
+    part1, part2, part3 = [], [], []
+    if len(rows) <= 8192 - size:
+        part1 = rows
+    else:
+        part1, rows = rows[:8192-size], rows[8192-size:]
+        if len(rows) <= 8192:
+            part2 = rows
+        else:
+            part2, part3 = rows[:8192], rows[8192:]
+
+    assert len(part3) <= 32640
+
+    size += len(part1)  # compressed data
+
+    print('Tiles size:', size)
+    fout.write(struct.pack('I', size << 8 | CHUNK_TILES))
 
     fout.write(struct.pack('H', DIAMETER))
-    for color, count in rows:
-        fout.write(bytes([count << 4 | color]))
+    fout.write(struct.pack('H', data_size))
+    fout.write(bytes(compute_palette(COLORS)[:2*PALETTE_SIZE]))
+
+    fout.write(bytes(part1))
+    if part2:
+        print('Sprites size:', len(part2))
+        fout.write(struct.pack('I', len(part2) << 8 | CHUNK_SPRITES))
+        fout.write(bytes(part2))
+    if part3:
+        print('Map size:', len(part3))
+        fout.write(struct.pack('I', len(part3) << 8 | CHUNK_MAP))
+        fout.write(bytes(part3))
 
 
 def read_cartridge(filename):
@@ -141,12 +187,12 @@ def main():
 
     source = PIL.Image.open(sys.argv[1])
     target = map_to_cylinder(source, (WIDTH, HEIGHT))
-    target = target.filter(PIL.ImageFilter.GaussianBlur(1))
+    # target = target.filter(PIL.ImageFilter.GaussianBlur(1))
     target = convert_colors(target, COLORS)
 
     target.save('transformed.png')
 
-    data = compress_rows(target)
+    data = compress_bits(target)
     write_cartridge(sys.argv[2], data, COLORS)
 
 
